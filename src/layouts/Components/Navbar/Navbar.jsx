56fbc6f7d406cd88e0963f17a6b0bd154235539c
Sagun <sagunsaluja13@gmail.com>
2023-05-25 08:35:11 +0530

    added navbar

--- src/layouts/Components/Navbar/Navbar.jsx
+++ src/layouts/Components/Navbar/Navbar.jsx
@@ -3,13 +3,15 @@ import { Link } from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
-// import Typography from "@mui/material/Typography";
 import styled from "styled-components";
 import logo from "../../../utils/img/logo.png";
+import { styled as muiStyled } from "@mui/system";
+import { useLocation } from "react-router-dom";
 
 const NavBarContainer = styled.div({
-  flexGrow: 1,
-  fontSize: "15px",
+  display: "flex",
+  width: "100%",
+  justifyContent: "space-between",
 });
 
 const Logo = styled.img({
@@ -21,12 +23,52 @@ const Logo = styled.img({
 const LogoWrapper = styled.div({
   width: "14rem",
   marginRight: "2rem",
+  marginTop: "0.3rem",
 });
+
+const ButtonWrapper = styled.div({
+  justifyContent: "flex-start",
+  width: "auto",
+  textAlign: "left",
+  display: "flex",
+  flex: "1 1",
+});
+
+const MenuWrapper = styled.div({
+  justifyContent: "flex-end",
+  display: "flex",
+  height: "95%",
+  marginBottom: "0.3rem",
+});
+
+const CustomButton = muiStyled(Button)((props) => ({
+  color: props?.selected ? "#1976d2" : "black",
+  marginRight: "1rem",
+  fontSize: "0.75rem",
+}));
+
+const CustomMenuButton = muiStyled(Button)((props) => ({
+  color: props?.login ? "black" : "white",
+  fontSize: "0.75rem",
+  background: props?.login ? "#f3f0ec" : "#1976d2",
+  paddingLeft: "16px",
+  paddingRight: "16px",
+  height: "100%",
+  borderRadius: "0px",
+  fontWeight: props?.login ? "400" : "700",
+  "&:hover": {
+    backgroundColor: "#383e45",
+    color: "#fff",
+    fontWeight: "700",
+  },
+}));
 
 const Navbar = () => {
   const handleClick = () => {
     localStorage.removeItem("Token");
   };
+  let location = useLocation();
+  console.log("location.pathname", location.pathname);
   return (
     <NavBarContainer>
       <AppBar position="fixed" sx={{ background: "#fff", height: "3.75rem" }}>
@@ -36,27 +78,65 @@ const Navbar = () => {
               <Logo src={logo} alt="logo" />
             </Link>
           </LogoWrapper>
+          <ButtonWrapper>
+            <Link to="/password-generator">
+              <CustomButton
+                selected={location.pathname === "/password-generator"}
+                size="small"
+                disableFocusRipple
+                disableRipple
+              >
+                Password Generator
+              </CustomButton>
+            </Link>
 
-          <Link to="/trainee">
-            <Button sx={{ color: "black" }}>TRAINEE</Button>
-          </Link>
+            <Link to="/bmi-Calculator">
+              {" "}
+              <CustomButton
+                selected={location.pathname === "/bmi-Calculator"}
+                size="small"
+                disableFocusRipple
+                disableRipple
+              >
+                BMI Calculator
+              </CustomButton>
+            </Link>
 
-          <Link to="/textfield-demo">
-            {" "}
-            <Button sx={{ color: "black" }}>TEXTFIELD DEMO </Button>
-          </Link>
+            <Link to="/currency-converter">
+              <CustomButton
+                selected={location.pathname === "/currency-converter"}
+                size="small"
+                disableFocusRipple
+                disableRipple
+              >
+                Currency Converter
+              </CustomButton>
+            </Link>
 
-          <Link to="/input-demo">
-            <Button sx={{ color: "black" }}>INPUT DEMO</Button>
-          </Link>
-
-          <Link to="/children-demo">
-            <Button sx={{ color: "black" }}>CHILDREN DEMO</Button>
-          </Link>
-
-          <Link to="/login" onClick={() => handleClick()}>
-            <Button sx={{ color: "black" }}> LogOut</Button>
-          </Link>
+            <Link to="/image-compressor">
+              <CustomButton
+                selected={location.pathname === "/image-compressor"}
+                size="small"
+                disableFocusRipple
+                disableRipple
+              >
+                Image Compressor
+              </CustomButton>
+            </Link>
+          </ButtonWrapper>
+          <MenuWrapper>
+            <Link to="/login" onClick={() => handleClick()}>
+              <CustomMenuButton login size="small" disableRipple>
+                Log In
+              </CustomMenuButton>
+            </Link>
+            <Link to="/sign-up" onClick={() => handleClick()}>
+              <CustomMenuButton size="small" disableRipple>
+                {" "}
+                Sign Up
+              </CustomMenuButton>
+            </Link>
+          </MenuWrapper>
         </Toolbar>
       </AppBar>
     </NavBarContainer>
