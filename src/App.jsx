50d15fbc32160f7c94a1ed9056c25239194d8b09
Sagun <sagunsaluja13@gmail.com>
2023-05-25 17:30:37 +0530

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -20,10 +20,42 @@ function App() {
             <Redirect to="/home" />
           </Route>
           <AuthRoutes exact path="/login" component={"Login"} />
+          <AuthRoutes exact path="/register" component={"register"} />
           <PrivateRoutes path="/home" component={Home} />
           <PrivateRoutes
             path="/password-generator"
             component={PasswordGenerator}
+          />
+          <PrivateRoutes path="/bmi-calculator" component={"bmi-calculator"} />
+          <PrivateRoutes
+            path="/currency-convertor"
+            component={"currency-convertor"}
+          />
+          <PrivateRoutes
+            path="/image-compressor"
+            component={"image-compressor"}
+          />
+          <PrivateRoutes path="/file-convertor" component={"file-convertor"} />
+          <PrivateRoutes
+            path="/markdown-editor"
+            component={"<>markdown-editor</>"}
+          />
+          <PrivateRoutes path="/px-to-rem" component={"<>px-to-rem</>"} />
+          <PrivateRoutes
+            path="/pomodoro-timer"
+            component={"PasswordGenerator"}
+          />
+          <PrivateRoutes
+            path="/qr-code-generator"
+            component={"PasswordGenerator"}
+          />
+          <PrivateRoutes
+            path="/password-strength-checker"
+            component={"PasswordGenerator"}
+          />
+          <PrivateRoutes
+            path="/lorem-ipsum-generator"
+            component={"PasswordGenerator"}
           />
         </Switch>
       </Router>
